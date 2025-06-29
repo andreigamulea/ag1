9d989252ccd8973d45e885efdac10e84745d6808
andreigamulea <ayushcellromania@gmail.com>
2025-06-29 04:06:02 +0300

    select2

--- app/javascript/application.js
+++ app/javascript/application.js
@@ -2,17 +2,6 @@
 import "@hotwired/turbo-rails"
 import "controllers"
 import Rails from "@rails/ujs"
-
-
-import "jquery"
-console.log("jquery?", typeof $); // trebuie să zică "function"
-import "select2"
-
-document.addEventListener("turbo:load", () => {
-  $(".select2").select2();
-});
-
-
 Rails.start()
 
 
