91e51e8b5680dd4b48d5b8dcb80be582a3b0c07d
andreigamulea <ayushcellromania@gmail.com>
2025-07-01 04:26:17 +0300

    .

--- app/controllers/carti_controller.rb
+++ app/controllers/carti_controller.rb
@@ -1,10 +1,10 @@
 class CartiController < ApplicationController
   def index
     @products = Product
-      .joins(:categories)
-      .where(categories: { name: ['carte', 'fizic'] })
-      .group('products.id')
-      .having('COUNT(DISTINCT categories.id) = 2')
+    .joins(:categories)
+    .where(categories: { name: ['carte', 'fizic'] })
+    .group('products.id')
+    .having('COUNT(DISTINCT categories.name) >= 2')
   end
   def show
     @product = Product.find(params[:id])
