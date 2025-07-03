ee29bf721e4f11055ee9ecca80d16c1ef3b47dca
andreigamulea <ayushcellromania@gmail.com>
2025-07-03 03:05:00 +0300

    .

--- lib/memory_logger.rb
+++ lib/memory_logger.rb
@@ -1,14 +1,30 @@
 module MemoryLogger
   def self.fetch_memory_usage
     if RUBY_PLATFORM =~ /linux/
-      rss_kb = `ps -o rss= -p #{Process.pid}`.to_i
-      (rss_kb / 1024.0).round(2)
+      begin
+        status = File.read("/proc/self/status")
+        if match = status.match(/VmRSS:\s+(\d+)\s+kB/)
+          (match[1].to_i / 1024.0).round(2)
+        else
+          0.0
+        end
+      rescue => e
+        Rails.logger.error("MemoryLogger error: #{e.message}")
+        0.0
+      end
+
     elsif RbConfig::CONFIG['host_os'] =~ /mswin|mingw|cygwin/
-      require 'win32ole'
-      wmi = WIN32OLE.connect("winmgmts://")
-      processes = wmi.ExecQuery("select * from Win32_Process where ProcessId = #{Process.pid}")
-      memory = processes.each.first&.WorkingSetSize.to_i
-      (memory / 1024.0 / 1024.0).round(2)
+      begin
+        require 'win32ole'
+        wmi = WIN32OLE.connect("winmgmts://")
+        processes = wmi.ExecQuery("select * from Win32_Process where ProcessId = #{Process.pid}")
+        memory = processes.each.first&.WorkingSetSize.to_i
+        (memory / 1024.0 / 1024.0).round(2)
+      rescue => e
+        Rails.logger.error("MemoryLogger (Windows) error: #{e.message}")
+        0.0
+      end
+
     else
       0.0
     end
