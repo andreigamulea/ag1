062513400b3860a24dc7a2f0821acc522b6df12a
andreigamulea <ayushcellromania@gmail.com>
2025-07-03 07:24:40 +0300

    .

--- app/helpers/cdn_helper.rb
+++ app/helpers/cdn_helper.rb
@@ -1,12 +1,28 @@
 module CdnHelper
+  require 'net/http'
+  require 'uri'
+
   def bunny_storage_url(attachment)
-    return asset_path("fallbacks/no-image.jpg") unless attachment.present? && attachment.key
+    return asset_path("fallbacks/no-image.jpg") unless attachment&.key.present?
 
+    cdn_url = "https://ayus-cdn.b-cdn.net/#{attachment.key}"
 
-    original_url = url_for(attachment)
+    # Verifică rapid dacă CDN-ul servește imaginea (HEAD request)
+    uri = URI.parse(cdn_url)
+    response = nil
+    begin
+      response = Net::HTTP.start(uri.host, uri.port, use_ssl: uri.scheme == 'https') do |http|
+        http.head(uri.request_uri)
+      end
+    rescue
+      response = nil
+    end
 
-    
-    "https://ayus-cdn.b-cdn.net/#{attachment.key}/#{attachment.filename}"
-
+    if response&.code == "200"
+      cdn_url
+    else
+      # fallback la url-ul temporar generat de ActiveStorage (semnat)
+      url_for(attachment)
+    end
   end
 end
