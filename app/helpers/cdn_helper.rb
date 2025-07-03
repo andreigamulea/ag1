7f2172c036a1511c79b6252708d7c406f531662f
andreigamulea <ayushcellromania@gmail.com>
2025-07-03 06:24:04 +0300

    .

--- app/helpers/cdn_helper.rb
+++ app/helpers/cdn_helper.rb
@@ -1,13 +1,10 @@
 module CdnHelper
-  def bunny_cdn_url(attachment)
-    return asset_path("fallbacks/no-image.jpg") unless attachment&.attached?
+  def bunny_storage_url(attachment)
+    return asset_path("fallbacks/no-image.jpg") unless attachment.present? && attachment.key
+
 
     original_url = url_for(attachment)
 
-    if Rails.env.production?
-      original_url.gsub("https://ag1-eef1.onrender.com", "https://ayus-cdn.b-cdn.net")
-    else
-      original_url
-    end
+    "https://ayus.b-cdn.net/#{attachment.key}/#{attachment.filename}"
   end
 end
