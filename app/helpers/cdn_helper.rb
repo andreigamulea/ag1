e8a1c3a0fa7511a4de8786ac81c55d36643d16f1
andreigamulea <ayushcellromania@gmail.com>
2025-07-03 04:41:17 +0300

    .

--- app/helpers/cdn_helper.rb
+++ app/helpers/cdn_helper.rb
@@ -1,8 +1,8 @@
 # app/helpers/cdn_helper.rb
 module CdnHelper
   def cdn_image_url(attachment, resize: [200, 200])
-    variant = attachment.variant(resize_to_limit: resize)
-    key = Rails.application.routes.url_helpers.rails_representation_path(variant.processed, only_path: true)
-    "https://ayus-cdn.b-cdn.net#{key}"
+    variant = attachment.variant(resize_to_limit: resize).processed
+    path = Rails.application.routes.url_helpers.rails_representation_path(variant, only_path: true)
+    "https://ayus-cdn.b-cdn.net#{path}"
   end
 end
