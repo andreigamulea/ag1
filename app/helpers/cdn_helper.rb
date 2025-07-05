1083a58e6dfa90112acb2bb704da37a4af27577c
andreigamulea <ayushcellromania@gmail.com>
2025-07-06 00:56:29 +0300

    .

--- app/helpers/cdn_helper.rb
+++ app/helpers/cdn_helper.rb
@@ -1,4 +1,5 @@
 # app/helpers/cdn_helper.rb
+require 'faraday'
 module CdnHelper
   def bunny_storage_url(attachment)
     return asset_path("placeholder.png") unless attachment&.blob&.persisted?
@@ -8,8 +9,14 @@ module CdnHelper
       Rails.logger.debug "Generating CDN URL: #{cdn_url}"
       begin
         response = Faraday.head(cdn_url)
-        response&.code == "200" ? cdn_url : url_for(attachment)
-      rescue
+        if response&.status == 200
+          cdn_url
+        else
+          Rails.logger.error "CDN URL failed (status: #{response&.status}): #{cdn_url}"
+          url_for(attachment)
+        end
+      rescue Faraday::Error => e
+        Rails.logger.error "Error accessing CDN URL #{cdn_url}: #{e.message}"
         url_for(attachment)
       end
     end
