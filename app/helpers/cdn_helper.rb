3d1ef3e82972f6de9ec9e09c7e4f427281e83775
andreigamulea <ayushcellromania@gmail.com>
2025-07-05 18:33:52 +0300

    .

--- app/helpers/cdn_helper.rb
+++ app/helpers/cdn_helper.rb
@@ -1,27 +1,15 @@
 module CdnHelper
-  require 'net/http'
-  require 'uri'
-
   def bunny_storage_url(attachment)
-    return asset_path("fallbacks/no-image.jpg") unless attachment&.key.present?
+    return asset_path("placeholder.png") unless attachment&.key.present?
 
     cdn_url = "https://ayus-cdn.b-cdn.net/#{attachment.key}"
-
-    # Verifică rapid dacă CDN-ul servește imaginea (HEAD request)
-    uri = URI.parse(cdn_url)
-    response = nil
-    begin
-      response = Net::HTTP.start(uri.host, uri.port, use_ssl: uri.scheme == 'https') do |http|
+    Rails.cache.fetch("cdn_url_#{attachment.key}", expires_in: 1.hour) do
+      uri = URI.parse(cdn_url)
+      response = Net::HTTP.start(uri.host, uri.port, use_ssl: true, open_timeout: 2, read_timeout: 2) do |http|
         http.head(uri.request_uri)
       end
+      response&.code == "200" ? cdn_url : url_for(attachment)
     rescue
-      response = nil
-    end
-
-    if response&.code == "200"
-      cdn_url
-    else
-      # fallback la url-ul temporar generat de ActiveStorage (semnat)
       url_for(attachment)
     end
   end
