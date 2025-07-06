ca21ec91e1755d0c86ed11d497be9b3338299c34
andreigamulea <ayushcellromania@gmail.com>
2025-07-06 08:11:08 +0300

    .

--- app/controllers/uploads_controller.rb
+++ app/controllers/uploads_controller.rb
@@ -1,19 +1,19 @@
 class UploadsController < ApplicationController
-  def presign
-    zone = ENV["BUNNY_STORAGE_ZONE"]
-    api_key = ENV["BUNNY_STORAGE_API_KEY"]
-    raise "Zona de stocare Bunny nu e setată" unless zone && api_key
+ def presign
+  filename = params[:filename]
+  raise "Lipsă filename" if filename.blank?
 
-    filename = params[:filename]
-    raise "Lipsă filename" if filename.blank?
+  zone = ENV["BUNNY_STORAGE_ZONE"] || "ayus"
+  api_key = ENV["BUNNY_STORAGE_API_KEY"] || Rails.application.credentials.dig(:bunny, :storage_key)
 
-    upload_url = "https://storage.bunnycdn.com/#{zone}/#{filename}"
-    render json: {
-      upload_url: upload_url,
-      headers: {
-        "AccessKey": api_key,
-        "Content-Type": "application/octet-stream"
-      }
+  upload_url = "https://storage.bunnycdn.com/#{zone}/#{filename}"
+  render json: {
+    upload_url: upload_url,
+    headers: {
+      "AccessKey" => api_key,
+      "Content-Type" => "application/octet-stream"
     }
-  end
+  }
+end
+
 end
