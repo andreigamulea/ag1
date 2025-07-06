6339dde456fbc6d6245f09604bed2ef57e02cbae
andreigamulea <ayushcellromania@gmail.com>
2025-07-06 11:20:06 +0300

    .

--- app/controllers/carti_controller.rb
+++ app/controllers/carti_controller.rb
@@ -1,13 +1,13 @@
 class CartiController < ApplicationController
   def index
-    @products = Product.select(:id, :name, :price, :stock, :stock_status, :custom_attributes, :track_inventory)
+    @products = Product.select(:id, :name, :price, :stock, :stock_status, :custom_attributes, :track_inventory, :external_image_url)
                       .joins(:categories)
                       .where(categories: { name: ['carte', 'fizic'] })
                       .group('products.id')
                       .having('COUNT(DISTINCT categories.name) >= 2')
-                      .includes(:main_image_attachment, :secondary_images_attachments)
                       .page(params[:page]).per(20)
   end
+
 
   def show
     @product = Product.select(:id, :name, :price, :description, :stock, :stock_status, :custom_attributes, :track_inventory)
