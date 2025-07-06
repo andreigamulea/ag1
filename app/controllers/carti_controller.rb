d1081572cf626b2126ce965424fcbfb9557b60e4
andreigamulea <ayushcellromania@gmail.com>
2025-07-06 11:25:21 +0300

    .

--- app/controllers/carti_controller.rb
+++ app/controllers/carti_controller.rb
@@ -1,19 +1,17 @@
 class CartiController < ApplicationController
   def index
-    @products = Product.select(:id, :name, :price, :stock, :stock_status, :custom_attributes, :track_inventory, :external_image_url)
-                      .joins(:categories)
-                      .where(categories: { name: ['carte', 'fizic'] })
-                      .group('products.id')
-                      .having('COUNT(DISTINCT categories.name) >= 2')
-                      .page(params[:page]).per(20)
+    @products = Product
+      .select(:id, :name, :price, :stock, :stock_status, :custom_attributes, :track_inventory, :external_image_url)
+      .joins(:categories)
+      .where(categories: { name: ['carte', 'fizic'] })
+      .group('products.id')
+      .having('COUNT(DISTINCT categories.name) >= 2')
+      .page(params[:page]).per(20)
   end
 
-
   def show
-    @product = Product.select(:id, :name, :price, :description, :stock, :stock_status, :custom_attributes, :track_inventory)
-                     .includes(:main_image_attachment, :secondary_images_attachments)
-                     .find(params[:id])
-    Rails.logger.debug "Main image attached: #{@product.main_image.attached?}"
-    Rails.logger.debug "Secondary images count: #{@product.secondary_images.count}"
+    @product = Product
+      .select(:id, :name, :price, :description, :stock, :stock_status, :custom_attributes, :track_inventory, :external_image_url, :external_image_urls)
+      .find(params[:id])
   end
 end
