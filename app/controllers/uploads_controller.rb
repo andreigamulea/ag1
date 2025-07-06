b5ee842d446c539e6491055ff357b6359cac5ac2
andreigamulea <ayushcellromania@gmail.com>
2025-07-06 23:21:32 +0300

    .

--- app/controllers/uploads_controller.rb
+++ app/controllers/uploads_controller.rb
@@ -1,24 +1,19 @@
-require "marcel"
-
 class UploadsController < ApplicationController
-  def presign
-    filename = params[:filename]
-    raise "Lipsă filename" if filename.blank?
+ def presign
+  filename = params[:filename]
+  raise "Lipsă filename" if filename.blank?
 
-    zone = ENV["BUNNY_STORAGE_ZONE"] || "ayus"
-    api_key = ENV["BUNNY_STORAGE_API_KEY"] || Rails.application.credentials.dig(:bunny, :storage_key)
+  zone = ENV["BUNNY_STORAGE_ZONE"] || "ayus"
+  api_key = ENV["BUNNY_STORAGE_API_KEY"] || Rails.application.credentials.dig(:bunny, :storage_key)
 
-    # Determină tipul de fișier (ex: application/pdf)
-    content_type = Marcel::MimeType.for(filename) || "application/octet-stream"
-
-    upload_url = "https://storage.bunnycdn.com/#{zone}/#{filename}"
-
-    render json: {
-      upload_url: upload_url,
-      headers: {
-        "AccessKey" => api_key,
-        "Content-Type" => content_type
-      }
+  upload_url = "https://storage.bunnycdn.com/#{zone}/#{filename}"
+  render json: {
+    upload_url: upload_url,
+    headers: {
+      "AccessKey" => api_key,
+      "Content-Type" => "application/octet-stream"
     }
-  end
+  }
+end
+
 end
