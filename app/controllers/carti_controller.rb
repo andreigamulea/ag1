d84b7fbbaeca87a208802481f07165e14f64df26
andreigamulea <ayushcellromania@gmail.com>
2026-01-23 22:56:01 +0200

    v1

--- app/controllers/carti_controller.rb
+++ app/controllers/carti_controller.rb
@@ -6,15 +6,37 @@ class CartiController < ApplicationController
       .where(categories: { name: ['carte', 'fizic'] })
       .group('products.id')
       .having('COUNT(DISTINCT categories.name) >= 2')
-      .page(params[:page]).per(20)
+
+    # Filtrare adițională după categorie (opțional)
+    if params[:category].present?
+      @products = @products.joins(:categories).where(categories: { slug: params[:category] })
+      @current_category = Category.find_by(slug: params[:category])
+    end
+
+    @products = @products.page(params[:page]).per(20)
   end
 
   def show
-  @product = Product
-    .select(:id, :name, :price, :description, :stock, :stock_status, :custom_attributes, :track_inventory, :external_image_url, :external_image_urls, :meta_title, :meta_description, :slug)
-    .find(params[:id])
-  
-  # Verifică dacă există produse în coș
-  @cart_has_items = session[:cart].present? && session[:cart].any?
-end
+    @product = Product
+      .select(:id, :name, :price, :description, :stock, :stock_status, :custom_attributes, :track_inventory, :external_image_url, :external_image_urls, :meta_title, :meta_description, :slug)
+      .find(params[:id])
+
+    # Verifică dacă există produse în coș
+    @cart_has_items = session[:cart].present? && session[:cart].any?
+
+    # Produse similare (din aceleași categorii, exclus produsul curent)
+    category_ids = @product.category_ids
+    if category_ids.present?
+      @similar_products = Product
+        .select(:id, :name, :price, :external_image_url)
+        .joins(:categories)
+        .where(categories: { id: category_ids })
+        .where.not(id: @product.id)
+        .where(status: 'active')
+        .distinct
+        .limit(4)
+    else
+      @similar_products = []
+    end
+  end
 end
